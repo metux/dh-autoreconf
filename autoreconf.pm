35c150c023333d1cfc768a80fc2076a7b07a6ca2
Julian Andres Klode <jak@debian.org>
2018-01-24 22:26:22 +0100

    Run dh_autoreconf after dh_update_autotools_config, not before dh_auto_configure

--- autoreconf.pm
+++ autoreconf.pm
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_before("dh_auto_configure", "dh_autoreconf");
+insert_after("dh_update_autotools_config", "dh_autoreconf");
 insert_before("dh_clean", "dh_autoreconf_clean");
 
 1;
